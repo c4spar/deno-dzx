247713a227087c49620cacf42509b03be93e254a
Benjamin Fischer <c4spar@gmx.de>
2021-05-08 15:28:12 +0200

    refactor: remove example file

--- example.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env deno run --allow-run https://deno.land/x/dzx/dzx.ts
-/// <reference path="https://deno.land/x/dzx/types.d.ts" />
-
-console.log(
-  "output: %s",
-  await $`echo Hello ${$.blue.bold("world")}!`,
-);
