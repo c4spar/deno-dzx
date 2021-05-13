111d41c65e21d285341d0668436df44e24b3b6f3
Benjamin Fischer <c4spar@gmx.de>
2021-05-13 15:06:00 +0200

    fix: make cd always relative to starting cwd

--- src/runtime/cd.ts
+++ src/runtime/cd.ts
@@ -1,11 +1,13 @@
 import { error } from "../_utils.ts";
 
+const cwd = Deno.cwd();
+
 export function cd(dir: string) {
   if ($.verbose) {
     console.log($.brightBlue("$ %s"), `cd ${dir}`);
   }
   try {
-    Deno.chdir(dir);
+    Deno.chdir(new URL(dir, path.toFileUrl(cwd + path.sep)).pathname);
   } catch (err) {
     if (err instanceof Deno.errors.NotFound) {
       const stack: string = (new Error().stack!.split("at ")[2]).trim();
