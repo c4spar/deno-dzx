6df1f851a6e4faa4e9408f44cef940a33b962821
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2021-08-12 16:05:31 -0700

    chore(deps): bump dependency versions

--- src/runtime/deps.ts
+++ src/runtime/deps.ts
@@ -20,5 +20,5 @@ export type {
   ArgParsingOptions,
   Args,
 } from "https://deno.land/std@0.104.0/flags/mod.ts";
-export { colors } from "https://deno.land/x/cliffy@v0.18.2/ansi/colors.ts";
+export { colors } from "https://deno.land/x/cliffy@v0.19.5/ansi/colors.ts";
 export { default as shq } from "https://esm.sh/shq@1.0.2";
