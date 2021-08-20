e3f1cfc0cbc3a376eeaf7fa6e70b23374383a25e
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2021-08-19 18:11:33 -0700

    refactor(os): remove os related methods added

--- src/_utils.ts
+++ src/_utils.ts
@@ -13,31 +13,3 @@ export function error(message: string | Error, exitCode = 1): Error {
 function getErrorMessage(message: string) {
   return $.red(`${$.bold("error:")} ${message}`);
 }
-
-export function homedir(throwIfMissing = true): string | undefined {
-  switch (Deno.build.os) {
-    case "windows":
-      return Deno.env.get("USERPROFILE");
-    case "linux":
-    case "darwin":
-      return Deno.env.get("HOME");
-    default:
-      if (throwIfMissing) {
-        throw error("Failed to retrieve home directory.");
-      }
-  }
-}
-
-export function username(throwIfMissing = true): string | undefined {
-  switch (Deno.build.os) {
-    case "windows":
-      return Deno.env.get("USERNAME");
-    case "linux":
-    case "darwin":
-      return Deno.env.get("USER");
-    default:
-      if (throwIfMissing) {
-        throw error("Failed to retrieve username.");
-      }
-  }
-}
