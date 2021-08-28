717c7f3d2a72198da7990fcc45ac8bce269ebb73
Benjamin Fischer <c4spar@gmx.de>
2021-08-28 23:09:10 +0200

    fmt

--- src/runtime/process_test.ts
+++ src/runtime/process_test.ts
@@ -128,7 +128,11 @@ Deno.test({
   name: "[process] should iterate over stdout line by line",
   async fn() {
     const lines: Array<string> = [];
-    for await (const line of new Process("echo foo; echo bar >&2; echo baz; echo baz >&2")) {
+    for await (
+      const line of new Process(
+        "echo foo; echo bar >&2; echo baz; echo baz >&2",
+      )
+    ) {
       lines.push(line);
     }
     assertEquals(lines, ["foo", "baz"]);
