f3d513ae55ee48834177717cdfcb6ca826827f4d
Benjamin Fischer <61995275+c4spar@users.noreply.github.com>
2021-08-28 18:28:39 +0200

    test(runtime): add process output and process error test's (#11)

---
+++ src/runtime/process_output_test.ts
@@ -0,0 +1,54 @@
+import { assert, assertObjectMatch } from "../../dev_deps.ts";
+import { $, ProcessOutput } from "./mod.ts";
+
+function createOutput(): ProcessOutput {
+  return new ProcessOutput({
+    stdout: "foo",
+    stderr: "bar",
+    combined: "foobaz",
+    status: {
+      code: 0,
+      success: true,
+    },
+  });
+}
+
+Deno.test({
+  name: "[process error] should have all properties defined",
+  fn() {
+    const output = createOutput();
+    assertObjectMatch(output, {
+      stdout: "foo",
+      stderr: "bar",
+      combined: "foobaz",
+      status: {
+        code: 0,
+        success: true,
+      },
+    });
+  },
+});
+
+Deno.test({
+  name: "[process error] should return an instance of process output",
+  async fn() {
+    const output = await $`exit 0`;
+    assert(output instanceof ProcessOutput);
+  },
+});
+
+Deno.test({
+  name: "[process error] should return an instance of process output",
+  async fn() {
+    const output = await $`echo foo; echo bar >&2`;
+    assertObjectMatch(output, {
+      stdout: "foo\n",
+      stderr: "bar\n",
+      combined: "foo\nbar\n",
+      status: {
+        code: 0,
+        success: true,
+      },
+    });
+  },
+});
