5b7fe67c2152c49c4f6bc2d19d0b87149079f09f
Benjamin Fischer <61995275+c4spar@users.noreply.github.com>
2022-07-17 19:24:27 +0200

    docs: refactor docs (#93)

--- src/runtime/mod.ts
+++ src/runtime/mod.ts
@@ -1,5 +1,7 @@
+import { $ } from "./shell.ts";
 export { $, $e, $o } from "./shell.ts";
 export { cd } from "./cd.ts";
 export { quote } from "./quote.ts";
 export { ProcessError } from "./process_error.ts";
 export { ProcessOutput } from "./process_output.ts";
+export default $;
